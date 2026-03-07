f81480c53fcd679a3ccfcc6830acecbafc49f54a
costajoaopedro652-glitch <costajoaopedro652@gmail.com>
2026-03-07 14:27:57 -0300

    imigração de tabelas e criação de models

---
+++ database/migrations/2026_03_07_171659_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();// // quarto que fez o pedido
+            $table->enum('status',['pendente','confirmado','preparando','entregue']);//situação do pédido
+            $table->integer('estimated_time')->nullable()->default('pendente'); // tempo estimado em minutos
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
