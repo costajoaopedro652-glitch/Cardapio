5c74223fac5421498ca05a14de9f399a07366737
costajoaopedro652-glitch <costajoaopedro652@gmail.com>
2026-03-08 10:58:33 -0300

    criação da rota resource

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function (){
-    Route::resource('book',BookController::class);
+    Route::resource('items',ItemController::class);
 });
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
